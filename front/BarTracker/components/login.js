24cc76e30e24676f3fb2d2cfbaab4681ae59139a
Tim Moon <timhgmoon@gmail.com>
2023-04-25 12:49:14 -0700

    added basic login layout. Still need basic color scheme and spacing

--- front/BarTracker/components/login.js
+++ front/BarTracker/components/login.js
@@ -1,10 +1,38 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import styles from '../styles/login';
 
 export default function Login() {
+
+    const [username, onChangeUsername] = React.useState('Username')
+    const [password, onChangePassword] = React.useState('Password')
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Login Screen!</Text>
+        <View style={styles.container}>
+            <View style={styles.logincontainer} >
+                
+            <Text>Login Title</Text>
+            <TextInput
+                style={styles.textinput}
+                selectTextOnFocus
+                onChangeText={onChangeUsername}
+                value={username}
+            >
+            </TextInput>
+            <TextInput
+                style={styles.textinput}
+                selectTextOnFocus
+                onChangeText={onChangePassword}
+                value={password}
+            >
+            </TextInput>
+
+            <Text style={styles.login}>
+                Log In
+            </Text>
+            <View>
+                <Text>Forgot username | password</Text>
+            </View>
+            </View>
         </View>
     )
 }
---
+++ front/BarTracker/styles/login.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#cfeb34",
+    },
+
+    logincontainer: { 
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        padding: 15,
+        height: '75%',
+        width: '85%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#53eb34',
+    },
+
+    textinput: {
+        height: 40,
+        width: 150,
+        margin: 5,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: "#cfeb34",
+    },
+
+    login: {
+        height: 40,
+        textAlign: 'center',
+        width: 150,
+        borderColor: 'white',
+        borderWidth: 1,
+        margin: 10,
+        padding: 10,
+    }
+})
